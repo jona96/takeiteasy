4d4c729580adb245d2ffbd32d85dfaac138f926a
Jonathan <jonathan.hamm@outlook.de>
2024-03-09 21:22:17 +0100

    add game class

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
+import random
+
+class Game:
+
+    _all_tiles = [
+        (1, 2, 3), (1, 2, 4), (1, 2, 8),
+        (1, 6, 3), (1, 6, 4), (1, 6, 8),
+        (1, 7, 3), (1, 7, 4), (1, 7, 8),
+        (5, 2, 3), (5, 2, 4), (5, 2, 8),
+        (5, 6, 3), (5, 6, 4), (5, 6, 8),
+        (5, 7, 3), (5, 7, 4), (5, 7, 8),
+        (9, 2, 3), (9, 2, 4), (9, 2, 8),
+        (9, 6, 3), (9, 6, 4), (9, 6, 8),
+        (9, 7, 3), (9, 7, 4), (9, 7, 8),
+    ]
+
+    def __init__(self):
+        self.left_tiles = Game._all_tiles
+
+    def pick_tile(self):
+        if not any(self.left_tiles):
+            return None
+        index_of_tile = int(random.random() * len(self.left_tiles))
+        return self.left_tiles.pop(index_of_tile)
 
 
+if __name__ == '__main__':
+    game = Game()
 
-tiles = [
-    (1, 2, 3),
-    (1, 2, 4),
-    (1, 2, 8),
-    (1, 6, 3),
-    (1, 6, 4),
-    (1, 6, 8),
-    (1, 7, 3),
-    (1, 7, 4),
-    (1, 7, 8),
-    (5, 2, 3),
-    (5, 2, 4),
-    (5, 2, 8),
-    (5, 6, 3),
-    (5, 6, 4),
-    (5, 6, 8),
-    (5, 7, 3),
-    (5, 7, 4),
-    (5, 7, 8),
-    (9, 2, 3),
-    (9, 2, 4),
-    (9, 2, 8),
-    (9, 6, 3),
-    (9, 6, 4),
-    (9, 6, 8),
-    (9, 7, 3),
-    (9, 7, 4),
-    (9, 7, 8),
-]
-
-
-
-print(tiles)
-print(len(tiles))
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
+    print(game.pick_tile())
