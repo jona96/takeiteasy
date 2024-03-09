1d3bef0aeb477c22c2067c56a37cee9369089abc
Jonathan <jonathan.hamm@outlook.de>
2024-03-09 23:44:31 +0100

    add game class
    ending the game doesn't work yet

--- main.py
+++ main.py
@@ -183,12 +183,57 @@ class Board:
             score += row_from_right_score(row)
         return score
 
+    def tiles(self) -> dict[str, Tile]:
+        return self.tiles
+
+class GameNotRunningException(Exception):
+    pass
+class GameCannotPlaceTileException(Exception):
+    pass
+
+class Game:
+    
+    def __init__(self) -> None:
+        self.tiles = Tiles()
+        self.board = Board()
+        self.started = False
+        self._finished = False
+        self.current_tile = None
+
+    def start(self):
+        self.started = True
+        self.current_tile = self.tiles.pick_tile()
+        print(self.board.draw())
+
+    def get_tile(self) -> Tile:
+        if not self.started:
+            raise GameNotRunningException()
+        return self.current_tile
+    
+    def place_tile(self, position: str):
+        if self.finished(): return
+        try:
+            self.board.place_tile(self.current_tile, position)
+        except Exception as e: raise GameCannotPlaceTileException(e)
+        
+        self.current_tile = self.tiles.pick_tile()
+        
+        if len(self.board.tiles()) == 19:
+            self._finished = True
+
+    def finished(self) -> bool:
+        return self._finished
+
 if __name__ == '__main__':
-    tiles = Tiles()
 
-    board = Board()
-    board.draw()
-
-    for _ in range(19):
-        tile = tiles.pick_tile()
-        board.place_tile(tile, input(f'where to place {tile}? '))
+    game = Game()
+    game.start()
+    while not game.finished():
+        print(f'The current tile is: {game.get_tile()}')
+        position = input('position for current Tile (e.g. C3): ')
+        try:
+            game.place_tile(position)
+        except:
+            print(f'Could not place the tile on {position}. Try again.')
+    
+        
