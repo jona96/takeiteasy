f5018e275d815fe2c33ae45c4633cddcb1a30295
Jonathan <jonathan.hamm@outlook.de>
2024-03-27 23:39:48 +0100

    implement new place_tile function

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from board import Board
+from board import Board, BoardPosition
 from tiles import Tile, Tiles
 
 class GameNotRunningException(Exception): pass
@@ -22,6 +22,17 @@ class Game:
         if not self.started:
             raise GameNotRunningException()
         return self.current_tile
+    
+    def place_tile(self, position: BoardPosition):
+        if self.finished(): return
+        try:
+            self.board.place_tile(self.current_tile, position)
+        except Exception as e: raise GameCannotPlaceTileException(e)
+        
+        self.current_tile = self.tiles.pick_tile()
+        
+        if len(self.board.tiles()) == 19:
+            self._finished = True
     
     def place_tile_old(self, position: str):
         if self.finished(): return
