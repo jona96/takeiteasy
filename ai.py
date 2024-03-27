ec11a61a751b04b3faf922c5418209e99ea6da70
Jonathan <jonathan.hamm@outlook.de>
2024-03-27 23:28:54 +0100

    implement basic ai with random moves

--- ai.py
+++ ai.py
@@ -1,8 +1,32 @@
 
 from tiles import Tile, Tiles
 from board import Board
+import random
 
 class AI:
     
-    def run(self, board_state: Board, tile: Tile):
-        pass
+    def run(self, board: Board, tile: Tile) -> str:
+        column = random.choice(board.all_columns())
+        row = random.choice(board.all_rows(column))
+        position = f'{column}{row}'
+        if position in board.tiles().keys():
+            return self.run(board, tile)
+        else:
+            return position
+    
+
+if __name__ == '__main__':
+    from game import Game
+    from time import sleep
+    
+    game = Game()
+    game.start()
+    
+    ai = AI()
+    
+    for _ in range(19):
+        position = ai.run(game.board, game.get_tile())
+        print(f'place Tile {game.get_tile()} at {position}')
+        game.place_tile(position)
+        sleep(1)
+    
