347b2756142b339645a1c38ec5b2ed1622e53eef
Jonathan <jonathan.hamm@outlook.de>
2024-03-28 15:12:13 +0100

    make tiles set fixed

--- test.py
+++ test.py
@@ -293,10 +293,38 @@ class TestAI(unittest.TestCase):
     def test_score_1(self):
         """At least score 100 at depth 2"""
         board = Board()
-        tiles = Tiles()
+        tiles = [
+            Tile(5,2,3),
+            Tile(1,6,8),
+            Tile(5,2,4),
+            Tile(1,6,3),
+            Tile(9,6,4),
+            Tile(9,7,8),
+            Tile(5,7,8),
+            Tile(5,6,8),
+            Tile(5,2,8),
+            Tile(9,2,4),
+            Tile(5,6,3),
+            Tile(5,6,4),
+            Tile(1,2,4),
+            Tile(9,6,3),
+            Tile(1,7,8),
+            Tile(9,7,4),
+            Tile(5,7,3),
+            Tile(9,2,8),
+            Tile(1,2,3),
+            Tile(1,6,4),
+            Tile(9,2,3),
+            Tile(9,6,8),
+            Tile(9,7,3),
+            Tile(1,7,3),
+            Tile(1,2,8),
+            Tile(5,7,4),
+            Tile(1,7,4),
+        ]
         
         while any(board.open_positions()):
-            tile = tiles.pick_tile() # TODO: use fixed set of tiles
+            tile = tiles.pop()
             position = AI.get_best_position(board, tile, depth=2)
             board.place_tile(tile, position)
             board.draw()
