79a4da706101d3c72fa95ddd6d50b7c7ecc6b43a
Jonathan <jonathan.hamm@outlook.de>
2024-03-28 01:27:32 +0100

    implement ai

--- ai.py
+++ ai.py
@@ -1,6 +1,8 @@
 
 from tiles import Tile, Tiles
 from board import Board, BoardPosition
+from copy import deepcopy
+from statistics import mean
 import random
 
 class AI:
@@ -8,12 +10,40 @@ class AI:
     def _random_pick(self, board: Board, tile: Tile) -> BoardPosition:
         position = random.choice(board.ALL_POSITIONS)
         if position in board.tiles().keys():
-            return self.run(board, tile)
+            return self.get_best_position(board, tile)
         else:
             return position
-        
-    def run(self, board: Board, tile: Tile) -> BoardPosition:
-        return self._random_pick(board, tile)
+    
+    @staticmethod
+    def estimated_score(board: Board) -> float:
+        if len(board.tiles()) == 19:
+            return board.score()
+        else:
+            # try every left tile
+            scores = []
+            for tile in board.left_tiles():
+                for position in board.open_positions():
+                    board_copy = deepcopy(board)
+                    board_copy.place_tile(tile, position)
+                    scores.append(AI.estimated_score(board_copy))
+            return mean(scores)
+
+    def _try_everything(self, board: Board, tile: Tile) -> BoardPosition:
+        score_positions = {} # estimated score : BoardPosition
+        for position in board.open_positions():
+            board_copy = deepcopy(board)
+            board_copy.place_tile(tile, position)
+            score_positions[AI.estimated_score(board_copy)] = position
+        print(score_positions)
+        max_score = max(score_positions.keys())
+        best_position = score_positions(max_score)
+        return best_position
+
+    def get_best_position(self, board: Board, tile: Tile) -> BoardPosition:
+        if len(board.left_tiles()) <= 1:
+            return self._try_everything(board, tile)
+        else:
+            return self._random_pick(board, tile)
 
 if __name__ == '__main__':
     from game import Game
@@ -25,8 +55,8 @@ if __name__ == '__main__':
     ai = AI()
     
     for _ in range(19):
-        position = ai.run(game.board, game.get_tile())
+        position = ai.get_best_position(game.board, game.get_tile())
         print(f'place Tile {game.get_tile()} at {position}')
         game.place_tile(position)
-        sleep(1)
+        sleep(0.3)
     
