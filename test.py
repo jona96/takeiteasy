6507dc5f1b36fe2d504b1b10ebcadbecd00aa81b
Jonathan <jonathan.hamm@outlook.de>
2024-03-28 12:22:42 +0100

    refactor test env

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
 import unittest
 from ai import AI
 from board import Board, BoardPosition
+Pos = BoardPosition.from_string
 from tiles import Tile
 
+
+def board_A() -> Board:
+    board = Board()
+    board.place_tile(Tile(9,7,8), Pos('A1'))
+    return board
 
 class TestAI(unittest.TestCase):
 
     def test_with_two_spaces(self):
-        # prepare board
-        board = Board()
-        board.place_tile(Tile(9,7,8), BoardPosition('A', 1))
-        
-        # run ai with (1,2,3)
-        suggested_position = AI.get_best_position(board, Tile(1,2,3))
-        self.assertEqual(suggested_position, BoardPosition('E', 3))
+        suggested_position = AI.get_best_position(board_A(), Tile(1,2,3))
+        self.assertEqual(suggested_position, Pos('E3'))
 
 if __name__ == '__main__':
     unittest.main()
