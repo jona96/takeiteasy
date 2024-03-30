e025c33b4d452f5987aa97777dd67d6a70cc8123
Jonathan <jonathan.hamm@outlook.de>
2024-03-30 23:23:31 +0100

    comments

--- ai.py
+++ ai.py
@@ -151,14 +151,14 @@ class AI:
                     # calculate propability for each possible number with remaining tiles
                     for number in range(1, 10):
                         remaining_tiles_with_number = [tile for tile in board.remaining_tiles() if getattr(tile, nx) == number]
-                        propability_factor = (len(remaining_tiles_with_number) / len(board.remaining_tiles())) ** len(group)
+                        propability_factor = (len(remaining_tiles_with_number) / len(board.remaining_tiles())) ** len(group)  # todo: Wahrscheinlichkeit ohne zurücklegen mit ! berechnen
                         score += number * len(group) * propability_factor
                 elif all_items_equal(number_list):
                     # so far all number the same
                     number = number_list[0]
                     missing_tiles = len(group) - len(number_list)
                     remaining_tiles_with_number = [tile for tile in board.remaining_tiles() if getattr(tile, nx) == number]
-                    propability_factor = (len(remaining_tiles_with_number) / len(board.remaining_tiles())) ** missing_tiles
+                    propability_factor = (len(remaining_tiles_with_number) / len(board.remaining_tiles())) ** missing_tiles  # todo: Wahrscheinlichkeit ohne zurücklegen mit ! berechnen
                     score += number * len(group) * propability_factor
                 else:
                     # different tiles -> no way to safe the row
